32fafd4c510f436490fe968870b2f91f6e857b58
Fabricio Valerio <fvr.valerio@gmail.com>
2019-09-01 15:45:15 -0400

    ReactSoBarberefoeRedux

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+import { minify } from 'terser';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Preencha seu nome!'),
+  email: Yup.string()
+  .email('Insira um email valdo')
+  .required('O email é obrigatório'),
+  password: Yup.string().min(6, 'Mínimo 6 caracteres!').required('A senha é obrigatória'),
+})
+
+
+// import { Container } from './styles';
+
+export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data)
+  }
+  return (
+    <>
+      <img src={logo} alt="GoBarber"/>
+
+      <Form schema={schema}onSubmit={handleSubmit}>
+        <Input name="name"placeholder="Nome completo" />
+        <Input name="email"type="email" placeholder="Seu email" />
+        <Input name="password"type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Sou Cadastrado!</Link>
+
+      </Form>
+
+    </>
+  );
+}
